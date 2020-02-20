2b1149f2adbe0465f72a14746d1c68863a628796
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-19 23:47:48 -0500

    added nav

---
+++ public/Javascript/nav.js
@@ -0,0 +1,42 @@
+const main = document.querySelector('.home');
+const nav = document.querySelector('.nav');
+
+const offset = main.offsetHeight-nav.offsetHeight;
+
+window.onscroll = () => {
+    if(window.pageYOffset>offset){
+        nav.classList.remove('bottom-nav');
+        nav.classList.add('top-nav');
+    }else{
+        nav.classList.add('bottom-nav');
+        nav.classList.remove('top-nav');
+    }
+}
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+    burger.addEventListener('click', ()=>{
+        //toggle
+        nav.classList.toggle('navActive')
+        //active links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+            }
+            
+            
+        });
+        //burger animation
+        burger.classList.toggle('toggle')
+
+    });
+}
+const app = ()=> {
+    navSlide()
+}
+
+app()
--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(express.static(path.join(__dirname, 'public')))
 
+/*
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 app.get("/contact", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'))
-});
+});*/
 
 app.listen(PORT, err => {
     if(err) console.log("ERROR", err)
